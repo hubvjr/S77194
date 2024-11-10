6f72ba0da67055226a8f862c82b65ea5dfb0a021
ASUS <ASUS@172.17.65.186>
2024-11-10 10:35:59 +0800

    Lab4 question 5 source code

---
+++ src/lab4_5/newpackage/Lab4_5_1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4_5.newpackage;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Lab4_5_1 {
+     public static void main(String[] args) {
+        
+        String distanceInput = JOptionPane.showInputDialog("Enter the distance(in Meter): ");
+        int distance = Integer.parseInt(distanceInput);
+        
+        String hoursInput = JOptionPane.showInputDialog("Enter time(in hours)");
+        int hours = Integer.parseInt(hoursInput);
+        String minutesInput = JOptionPane.showInputDialog("Enter time(in minutes)");
+        int minutes = Integer.parseInt(minutesInput);        
+        String secondsInput = JOptionPane.showInputDialog("Enter time(in seconds)");        
+        int seconds = Integer.parseInt(secondsInput); 
+        
+        int totalTimeInSeconds = (hours*3600) + (minutes * 60) + seconds;
+        double speedMetersPerSecond = distance / totalTimeInSeconds;
+        double speedKilometersPerHour = (distance / 1000.0) / (totalTimeInSeconds / 3600.0);
+        double speedMilesPerHour = (distance / 1609.0) / (totalTimeInSeconds / 3600.0);
+        
+        JOptionPane.showMessageDialog(null, String.format("Speed:\n%.2f M/S\n%.2f KM/H\n%.2f MPH", // M/S = meters per second,
+                speedMetersPerSecond,speedKilometersPerHour,speedMilesPerHour));                   // KM/H = kilometers per hour
+    }                                                                                              // MPH = miles per hour
+    
+}
